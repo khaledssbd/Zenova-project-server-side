1c755583ce6cf623ee3da063a5043361bcc1c62c
khaledssbd <khaledssbd@gmail.com>
2024-08-17 03:45:07 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,76 @@
+# Zenova
+
+## Live site:
+
+- [Zenova on firebase](https://glamspot-khaled.web.app)
+- [Zenova on vercel](https://glamspot-by-khaled.vercel.app)
+- [Zenova on surge](https://glamspot-by-khaled.surge.sh)
+- [Zenova on netlify](https://glamspot-by-khaled.netlify.app)
+
+## GitHub Repository:
+
+- [Client-Repository](https://github.com/khaledssbd/Zenova-project-client-side)
+- [Server-Repository](https://github.com/khaledssbd/Zenova-project-server-side)
+
+---
+
+# Project Overview:
+
+- Add your service to database and manage them
+- Book others service as a user
+- provide and update service status in database
+
+# Features
+
+- Loading data in a amazing way with no bug using TanStack Query
+- axiosSecure to force logout a hacker who wants to ge others data
+- Awesome dark theme support
+- react-responsive-carousel, aos Implementation for better UI
+- jspdf for pdf downlaod
+- Environment Variable configuration to save admin data from hackers
+- Regex with Valid email, Uppercase letters, lowercase letters and 6 characters in password
+- PrivateRoute to make the user login must
+- jwt configuration to configure security
+
+# npm Packages
+
+- npm i cookie-parser
+- npm i cors
+- npm i dotenv
+- npm i express
+- npm i jsonwebtoken
+- npm i mongodb
+- npm i nodemon
+
+---
+
+# Getting Started
+
+To run this React project on your local machine follow the instructions-
+
+### Prerequisites
+
+Before you begin, ensure you have the following installed on your local machine:
+
+- [Node.js](https://nodejs.org/en/download/) (which includes npm)
+- [Git](https://git-scm.com/)
+
+### Installation
+
+1. ```bash
+   git clone https://github.com/khaledssbd/Zenova-project-server-side
+   ```
+
+2. ```bash
+   cd Zenova-project-server-side
+   ```
+
+3. ```bash
+   npm install
+   ```
+
+4. ```bash
+   npm run dev
+   ```
+
+Open [http://localhost:7000](http://localhost:7000) to view it in the browser.
---
+++ index.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+// all config
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 7000;
+
+// all middleware
+const corsConfig = {
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'https://glamspot-khaled.web.app',
+    'https://glamspot-by-khaled.vercel.app',
+    'https://glamspot-by-khaled.surge.sh',
+    'https://glamspot-by-khaled.netlify.app',
+  ],
+  credentials: true,
+};
+app.use(cors(corsConfig));
+app.use(express.json());
+app.use(cookieParser());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2brfitt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+// my made middlewares
+
+const verifyToken = async (req, res, next) => {
+  const token = req?.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: 'unauthorized access' });
+  }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: 'unauthorized access' });
+    }
+    if (req?.query?.email !== decoded?.email) {
+      return res.status(403).send({ message: 'forbidden access' });
+    }
+    next();
+  });
+};
+
+const cookieOptions = {
+  httpOnly: true,
+  sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+  secure: process.env.NODE_ENV === 'production',
+};
+
+async function run() {
+  try {
+    const productCollection = client.db('zenovaDB').collection('products');
+
+    
+    
+    
+    // Send a ping to confirm a successful connection to DB
+    await client.db('admin').command({ ping: 1 });
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Hellow! From Zenova server owner Khaled');
+});
+
+app.listen(port, () => {
+  console.log(`Zenova server is running on port: ${port}`);
+});
